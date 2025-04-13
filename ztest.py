33dd52db77661a5e63e39b08177d37f42f7b1680
ahmedmshakil <ahmedmshakil1@gmail.com>
2025-04-14 00:28:49 +0600

    added import module

---
+++ ztest.py
@@ -0,0 +1,57 @@
+# all import modules
+# 
+# 
+# 
+# extrafrom itertools import accumulate
+# 
+# 
+# shakil_13
+# 
+# <<<<>>>>>
+
+import sys
+
+def iinp():
+    return int(sys.stdin.readline().strip())
+def linp():
+    return list(map(int, sys.stdin.readline().strip().split()))
+def string():
+    return sys.stdin.readline().strip()
+def lsinp():
+    return sys.stdin.readline().strip().split()
+def digit():
+    return [int(i) for i in (list(sys.stdin.readline().strip()))]
+def character():
+    return list(sys.stdin.readline().strip())
+
+
+
+from collections import Counter, defaultdict, deque
+from bisect import bisect_right, bisect_left
+from math import ceil, sqrt, gcd
+
+
+# extrafrom itertools import accumulate
+# 
+# 
+# shakil_13
+# 
+from itertools import accumulate, combinations_with_replacement
+from itertools import permutations, combinations
+from functools import reduce
+from heapq import heappush, heappop
+from operator import itemgetter, attrgetter
+from copy import deepcopy
+from random import randint, choice, shuffle
+from string import ascii_lowercase, ascii_uppercase, digits
+from time import time
+from sys import setrecursionlimit, stdin, stdout
+from array import array
+from io import BytesIO, IOBase
+from types import GeneratorType
+from queue import Queue
+from array import array
+from math import factorial
+from collections import defaultdict
+from collections import Counter
+from collections import deque
